add8263a034f93c6f32c7575765549639102f248
Vladimir Borovik <bobahbdb@gmail.com>
2017-01-07 23:10:37 +0300

    Added translation

---
+++ index.js
@@ -0,0 +1,30 @@
+const clipboard = require('clipboardy');
+const gt = require('google-translate-api');
+const notifier = require('node-notifier');
+const path = require('path');
+
+
+function notifyErr(err) {
+  notifier.notify({
+    title: 'Error',
+    message: err.message || 'Неизвестная ошибка',
+    icon: path.join(__dirname, 'media', 'error.png'),
+  }, (error) => {
+    if (error) console.log(error);
+  });
+}
+
+let originalText = '';
+clipboard.read()
+.then((data) => {
+  originalText = data;
+  return gt(data, { to: 'ru' });
+})
+.then((res) => {
+  notifier.notify({
+    title: originalText,
+    message: res.text,
+    icon: path.join(__dirname, 'media', 'success.png'),
+  });
+})
+.catch(err => notifyErr(err));
